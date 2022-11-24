45cb3a834257d188ea0b3156261bdcb9c48e7b65
manaspalaparthi <59759897+manaspalaparthi@users.noreply.github.com>
2022-11-24 23:02:02 +0530

    test run success

--- util/ocr/tessract_ocr.py
+++ util/ocr/tessract_ocr.py
@@ -2,9 +2,10 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import pandas as pd
 import pytesseract
 import re
+import datetime
 
 plt.rcParams['figure.figsize'] = [10, 10]
 
@@ -14,52 +15,60 @@ def load_frame(frame_number, cap):
     return(frame)
 
 def preprocess_frame(img):
-    img = img[10:50,610:670,::-1]
-    img = cv2.resize(img,(600,400))
+    img = img[170:205,150:240,::-1]
+    #img = cv2.resize(img,(600,400))
     img = 255 - cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return(img)
 
 
 def extract_angle_confidence(result_dict):
-    angle = np.NaN
+    temp = np.NaN
     confidence = np.NaN
     for i in range(0, len(result_dict['text'])):
         confidence = int(result_dict['conf'][i])
         if confidence > 0:
             text = result_dict['text'][i]
-            text = re.sub("[^0-9^]", "", text)
+            text = re.sub("[^0-9.^]", "", text)
             if len(text) > 0:
-                angle = int(text)
+                temp = text
 
-    return (angle, confidence)
+    return (temp, confidence)
 
 
-cap = cv2.VideoCapture('../../HYP_T_Data Files/HYP_T_12/Low Res Thermal Camera/20221028T105933.MP4')
+cap = cv2.VideoCapture('../../HYP_T_Data_Files/HYP_T_12/Low_Res_Thermal_Camera/20221028T105933.MP4')
 tesseract_config = r'--oem 3 --psm 13'
-img = load_frame(0,cap)
-result_dict = pytesseract.image_to_data(img, config = tesseract_config, output_type = pytesseract.Output.DICT)
+# img = load_frame(0,cap)
+# img = preprocess_frame(img)
+# result_dict = pytesseract.image_to_data(img, config = tesseract_config, output_type = pytesseract.Output.DICT)
 
-# angles = []
-# confidences = []
-# for i in range(0, 5_000):
-#     img = load_frame(i, cap)
-#     img = preprocess_frame(img)
-#
-#     tesseract_config = r'--oem 3 --psm 13'
-#     result_dict = pytesseract.image_to_data(img, config=tesseract_config, output_type=pytesseract.Output.DICT)
-#
-#     angle, confidence = extract_angle_confidence(result_dict)
-#
-#     angles.append(angle)
-#     confidences.append(confidence)
-#
-#
-# angles = np.array(angles)
-# confidences = np.array(confidences)
-#
-# np.save('angles.npy',angles)
-# np.save('confidences.npy',confidences)
+angles = []
+confidences = []
+frames = []
 
+count = 1
+while cap.isOpened():
+    res, frame = cap.read()
+    if res == True:
+        count =count +1
+        img = preprocess_frame(frame)
+        result_dict = pytesseract.image_to_data(img, config=tesseract_config, output_type=pytesseract.Output.DICT)
+        angle, confidence = extract_angle_confidence(result_dict)
+        print(f"temp {angle}")
+        frames.append(count)
+        angles.append(angle)
+        confidences.append(confidence)
+        # Press Q on keyboard to exit
+        cv2.imshow('Frame', frame)
+        cv2.imshow('"temp', img)
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+    else:
+        cv2.destroyAllWindows()
+        break
 
-plt.imshow(img[:,:,::-1])
-plt.show()
+dict = {"frame_number": frames,"temp": angles}
+
+df = pd.DataFrame(dict)
+
+df.to_csv('20221028T105933.csv')
